bb21a08b1def6db0bc36cabfcaa0c3cfe553c74f
anubhavtewari <anubhav.tewari94@gmail.com>
2024-05-30 19:13:36 +0530

    Goal parser

--- src/GoalParserInterpretation.java
+++ src/GoalParserInterpretation.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class GoalParserInterpretation {
+public class GoalParserInterpretation {
+        public String interpret(String command) {
+            String str = "";
+            for(int i = 0; i < command.length()-1; i++){
+                char ch = command.charAt(i);
+                char nextCh = command.charAt(i+1);
+                if(ch == 'G'){
+                    str = str + 'G';
+                }
+                else if(ch == '(' && nextCh == ')'){
+                    str = str + 'o';
+                }
+                else if(ch == '(' && nextCh == 'a'){
+                    str = str + "al";
+                }
+            }
+            if(command.charAt(command.length()-1) == 'G'){
+                str = str + 'G';
+            }
+            return str;
+        }
 }
