e4f6580c31b370795aaca82469af97402a87c4b6
anubhavtewari <anubhav.tewari94@gmail.com>
2024-05-31 22:33:52 +0530

    Added blank line in this code

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Calculation {
+public class Calculation {
+    int stockPrice = 10; // instance variable
+    static int carPrice = 55; // static instance variable
+    public static int getPenPrice(){
+
+        Calculation c1 = new Calculation();
+        c1.print();
+
+        carPrice = 100;
+      //  stockPrice = 10000; this can't be accessed because it is non-static, because it is associated with object
+        int capPrice = 10;
+        int bodyPrice = 25;
+        return capPrice + bodyPrice;
+
+
+    }
+
+    //print is non-static method. so every object has print() method
+    public int print(){ // this can't be static method as it is using an instance variable.
+        int p1 = 20 + stockPrice;
+        return p1;
+    }
+
 }
