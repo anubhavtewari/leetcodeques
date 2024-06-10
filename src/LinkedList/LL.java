e6ab249e25e30bc0d24d1831d3089e1e9808b056
anubhavtewari <anubhav.tewari94@gmail.com>
2024-06-10 12:41:46 +0530

    update changes

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -10,6 +10,19 @@ public class LL {
         this.size = 0;
     }
 
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.next = next;
+            this.value = value;
+        }
+    }
     public void insertFirst(int val){
         Node node = new Node(val);
         node.next = head;
@@ -58,18 +71,6 @@ public class LL {
         System.out.println("END");
     }
 
-    private class Node{
-        private int value;
-        private Node next;
 
-        public Node(int value){
-            this.value = value;
-        }
-
-        public Node(int value, Node next) {
-            this.next = next;
-            this.value = value;
-        }
-    }
 
 }
