32e5e94a373475f9f60af4376295f76380923fda
anubhavtewari <anubhav.tewari94@gmail.com>
2024-06-20 10:01:52 +0530

    Reverse LL using recursion

---
+++ src/LinkedList/ReverseLLRecursion.java
@@ -0,0 +1,37 @@
+//package LinkedList;
+//
+//public class ReverseLLRecursion {
+//
+//    /**
+//     * Definition for singly-linked list.
+//     * public class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode() {}
+//     *     ListNode(int val) { this.val = val; }
+//     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//     * }
+//     */
+//    class Solution {
+//        public ListNode reverseList(ListNode head) {
+//            // ListNode node = null;
+//            // while(head != null){
+//            //     ListNode temp = head.next;
+//            //     head.next = node;
+//            //     node = head;
+//            //     head = temp;
+//            // }
+//            // return node;
+//
+//            //recursion
+//            if(head == null || head.next == null){
+//                return head;
+//            }
+//
+//            ListNode newHead = reverseList(head.next);
+//            head.next.next = head;
+//            head.next = null;
+//            return newHead;
+//        }
+//    }
+//}
