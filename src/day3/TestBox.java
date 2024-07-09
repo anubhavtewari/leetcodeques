d8e8467446b5dd68a7ade4910728ad838559be15
anubhavtewari <anubhav.tewari94@gmail.com>
2024-07-09 08:01:02 +0530

    TB

--- src/day3/TestBox.java
+++ src/day3/TestBox.java
@@ -1,2 +1,19 @@
-package day3;public class TestBox {
+package day3;
+import java.util.Scanner;
+public class TestBox {
+    public static void main(String[] args) {
+        //create scanner cls instance to wrap stdin
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter side of a cube");
+        Box cube=new Box(sc.nextDouble());
+        System.out.println(cube.getBoxDetails());
+        //create dummy Box : inited with all dims -1
+        Box dummy=new Box();
+        System.out.println(dummy.getBoxDetails());
+        System.out.println("Enter box 1 dims");
+        Box box1=new Box(sc.nextDouble(),sc.nextDouble(),sc.nextDouble());
+        System.out.println("Enter box 2 dims");
+        Box box2=new Box(sc.nextDouble(),sc.nextDouble(),sc.nextDouble());
+        System.out.println(box1.isEqual(box2)?"Same":"Different");
+    }
 }
