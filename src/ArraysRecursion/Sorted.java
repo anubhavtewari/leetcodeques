8c77a299d21afa19fd543e9ba5369b9eabadfa77
anubhavtewari <anubhav.tewari94@gmail.com>
2024-07-22 00:02:03 +0530

    .

--- src/ArraysRecursion/Sorted.java
+++ src/ArraysRecursion/Sorted.java
@@ -1,2 +1,17 @@
-package ArraysRecursion;public class Sorted {
+package ArraysRecursion;
+
+public class Sorted {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,5,8,0,10};
+        System.out.println(sorted(arr,0));
+    }
+    static boolean sorted(int[] arr, int index){
+        // base condition
+        if(index == arr.length - 1){
+            return true;
+        }
+        return arr[index] < arr[index+1]  && sorted(arr, index+1);
+    }
 }
+
+
